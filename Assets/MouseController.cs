6483a3734e4afb4cdef5dd640dd1995fc0442cdf
lukas kastern <lukaskastern@web.de>
2018-12-09 12:02:50 +0100

    Cursor now locks to the center of the screen

---
+++ Assets/MouseController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    private void Awake()
+    {
+        LockMouse(CursorLockMode.Locked);
+        HideMouse(true);
+    }
+
+    public void LockMouse(CursorLockMode Status)
+    {
+        Cursor.lockState = Status;
+    }
+
+    public void HideMouse(bool Status)
+    {
+        Cursor.visible = Status;
+    }
+}
